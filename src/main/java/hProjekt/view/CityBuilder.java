b3bef85bdb13cbe601f31d404337fe9037464c31
Per <atlasfreak@gmx.de>
2024-10-03 04:04:47 +0200

    add basic builder

---
+++ src/main/java/hProjekt/view/CityBuilder.java
@@ -0,0 +1,55 @@
+package hProjekt.view;
+
+import hProjekt.model.City;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.TextAlignment;
+import javafx.util.Builder;
+
+public class CityBuilder implements Builder<Region> {
+    private final City city;
+    private final VBox pane = new VBox();
+    private Node marker;
+
+    public CityBuilder(final City city) {
+        this.city = city;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public double getMarkerWidth() {
+        return marker.getLayoutBounds().getWidth();
+    }
+
+    public double getMarkerHeight() {
+        return marker.getLayoutBounds().getHeight();
+    }
+
+    @Override
+    public Region build() {
+        pane.getChildren().clear();
+
+        final Circle circle = new Circle(10);
+        marker = circle;
+        circle.setStroke(Color.BLACK);
+        pane.getChildren().add(circle);
+
+        final Label text = new Label(city.getName());
+        text.setTextAlignment(TextAlignment.CENTER);
+        text.getStyleClass().add("highlighted-label");
+        pane.getChildren().add(text);
+
+        pane.setAlignment(Pos.CENTER);
+        // pane.setBackground(Background.fill(Color.WHITE));
+
+        return pane;
+    }
+
+}
---
+++ src/main/java/hProjekt/view/TileBuilder.java
@@ -0,0 +1,119 @@
+package hProjekt.view;
+
+import hProjekt.model.Tile;
+import javafx.css.CssMetaData;
+import javafx.css.Styleable;
+import javafx.css.StyleableProperty;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Builder;
+
+/**
+ * A Builder to create views for {@link Tile}s.
+ * Renders the {@link Tile} with a resource icon, a label for the roll number
+ * and the robber if present.
+ * Has methods to highlight and unhighlight the tile.
+ */
+public class TileBuilder implements Builder<Region> {
+    private final Tile tile;
+    private final StackPane pane = new StackPane();
+
+    /**
+     * Creates a new TileBuilder for the given {@link Tile}.
+     *
+     * @param tile the tile to render
+     */
+    public TileBuilder(final Tile tile) {
+        this.tile = tile;
+        styleAndSizeTile(pane);
+        // printComputedStyles(pane);
+    }
+
+    /**
+     * Returns the {@link Tile} this builder renders.
+     *
+     * @return the tile
+     */
+    public Tile getTile() {
+        return tile;
+    }
+
+    @Override
+    public Region build() {
+        pane.getChildren().clear();
+        final VBox mainBox = new VBox();
+        final StackPane resourcePane = new StackPane();
+
+        mainBox.getChildren().addAll(resourcePane, createLabels());
+        mainBox.setAlignment(Pos.CENTER);
+        pane.getChildren().addAll(mainBox);
+        return pane;
+    }
+
+    /**
+     * Styles and resizes the tile so it is rendered as a hexagon via css.
+     *
+     * @param stackPane
+     */
+    private void styleAndSizeTile(final StackPane stackPane) {
+        stackPane.getStylesheets().add("css/hexmap.css");
+        stackPane.getStyleClass().add("hex-tile");
+        stackPane.maxHeightProperty().bind(tile.heightProperty());
+        stackPane.maxWidthProperty().bind(tile.widthProperty());
+        stackPane.minHeightProperty().bind(tile.heightProperty());
+        stackPane.minWidthProperty().bind(tile.widthProperty());
+        stackPane.setBackground(Background.fill(tile.getType().color));
+    }
+
+    private void printComputedStyles(Region node) {
+        System.out.println("Computed styles for node:");
+        for (String styleClass : node.getStyleClass()) {
+            System.out.println("Style class: " + styleClass);
+            node.applyCss(); // Ensure CSS is applied
+            for (CssMetaData<? extends Styleable, ?> metaData : node.getCssMetaData()) {
+                StyleableProperty<?> styleableProperty = ((CssMetaData<Styleable, ?>) metaData)
+                        .getStyleableProperty(node);
+                System.out.println(metaData.getProperty() + ": " + styleableProperty.getValue());
+            }
+        }
+    }
+
+    /**
+     * Creates a VBox with labels for the position, resource and roll number of the
+     * tile.
+     *
+     * @return the created VBox
+     */
+    private VBox createLabels() {
+        final VBox labelBox = new VBox();
+        final Label positionLabel = new Label(tile.getPosition().toString());
+        positionLabel.getStyleClass().add("highlighted-label");
+        final Label resourceLabel = new Label(tile.getType().toString());
+        resourceLabel.getStyleClass().add("highlighted-label");
+
+        labelBox.setAlignment(Pos.CENTER);
+        return labelBox;
+    }
+
+    /**
+     * Highlights the tile and sets a handler for mouse clicks.
+     *
+     * @param hanlder the handler to call when the tile is clicked
+     */
+    public void highlight(final Runnable hanlder) {
+        pane.getStyleClass().add("selectable");
+        pane.setOnMouseClicked(e -> hanlder.run());
+    }
+
+    /**
+     * Removes the highlight and the handler for mouse clicks.
+     */
+    public void unhighlight() {
+        pane.getStyleClass().remove("selectable");
+        pane.setOnMouseClicked(null);
+    }
+}
