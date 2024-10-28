77d1b9284a26613da961bd1b0b8d1ae8e0d46d46
Per <atlasfreak@gmx.de>
2024-10-28 18:26:54 +0100

    add PlayerState

---
+++ src/main/java/hProjekt/model/PlayerState.java
@@ -0,0 +1,16 @@
+package hProjekt.model;
+
+import java.util.Set;
+
+import org.tudalgo.algoutils.student.annotation.DoNotTouch;
+
+/**
+ * Holds information on a player's state.
+ * that can be upgraded
+ *
+ * @param buildableRailEdges a set of edges where rail can be built
+ */
+@DoNotTouch
+public record PlayerState(
+        Set<Edge> buildableRailEdges) {
+}
