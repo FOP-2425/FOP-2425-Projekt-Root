5712ecfdaebcb79298b34f1292208bfabbf0d144
Henning Schmid <schmid.henning@googlemail.com>
2024-11-09 19:56:25 +0100

    Improved Frontend

--- src/main/java/hProjekt/view/menus/AboutBuilder.java
+++ src/main/java/hProjekt/view/menus/AboutBuilder.java
@@ -7,6 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.util.Builder;
 
@@ -25,16 +26,18 @@ public class AboutBuilder implements Builder<Region> {
         root.setAlignment(Pos.CENTER);
 
         // Logo
-        ImageView logo = new ImageView(new Image(getClass().getResourceAsStream("/images/dampfross_logo_black.png")));
+        ImageView logo = new ImageView(new Image(getClass().getResourceAsStream("/images/dampfross_logo_white.png")));
         logo.setFitWidth(300);
         logo.setPreserveRatio(true);
 
         Text gameDescription = new Text("Dampfross is a strategic train simulation game where you manage railway networks and expand your empire.");
         gameDescription.getStyleClass().add("text-description");
+        gameDescription.setFill(Color.WHITE);
 
         // Developer Information
         Text developerInfo = new Text("Developed by Technical University of Darmstadt, Department of Computer Science, Chair of Algorithms.");
         developerInfo.getStyleClass().add("text-subheading");
+        developerInfo.setFill(Color.WHITE); 
 
         // Credits
         Text credits = new Text("Credits:\n" +
@@ -42,20 +45,24 @@ public class AboutBuilder implements Builder<Region> {
                 "Designers: Alice, Bob\n" +
                 "Testing: Alice, Bob");
         credits.getStyleClass().add("text-credits");
+        credits.setFill(Color.WHITE);
 
         // License Information
         Text licenseInfo = new Text("Built using JavaFX.\n" +
                 "Licensed under the MIT License.");
         licenseInfo.getStyleClass().add("text-license");
+        licenseInfo.setFill(Color.WHITE); 
 
         // Acknowledgements
         Text acknowledgements = new Text("Special thanks to our dedicated supporters.");
         acknowledgements.getStyleClass().add("text-acknowledgements");
+        acknowledgements.setFill(Color.WHITE); 
 
         // Legal Information
         Text legalInfo = new Text("© 2024 TU Darmstadt Chair of Algorithms. All rights reserved.\n" +
                 "All trademarks are the property of their respective owners.");
         legalInfo.getStyleClass().add("text-legal");
+        legalInfo.setFill(Color.WHITE); 
 
         // Back to Main Menu Button
         Button backButton = new Button("Back to Main Menu");
@@ -66,6 +73,6 @@ public class AboutBuilder implements Builder<Region> {
         // Add css style
         root.getStylesheets().add(getClass().getResource("/css/about.css").toExternalForm());
 
-        return root; 
+        return root;
     }
 }
