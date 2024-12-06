38acedf2c61e0d30e5e056d97af9285c4c2f24e7
Per <atlasfreak@gmx.de>
2024-12-06 04:53:02 +0100

    fix: bind dash array correctly to outline

--- src/main/java/hProjekt/view/EdgeLine.java
+++ src/main/java/hProjekt/view/EdgeLine.java
@@ -4,7 +4,6 @@ import java.util.function.Consumer;
 
 import hProjekt.model.Edge;
 import hProjekt.model.EdgeImpl;
-import javafx.collections.ListChangeListener;
 import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -35,9 +34,8 @@ public class EdgeLine extends Line {
         outline.strokeDashOffsetProperty().bind(strokeDashOffsetProperty());
         outline.setStrokeWidth(strokeWidth * 1.4);
         outline.setStroke(Color.TRANSPARENT);
-        getStrokeDashArray().addListener((ListChangeListener<Double>) change -> {
-            outline.getStrokeDashArray().clear();
-            outline.getStrokeDashArray().addAll(change.getList());
+        getStrokeDashArray().subscribe(() -> {
+            outline.getStrokeDashArray().setAll(getStrokeDashArray());
         });
     }
 
