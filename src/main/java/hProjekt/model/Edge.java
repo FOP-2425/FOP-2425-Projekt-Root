d33309ef87a87c72c6fbc7fc9316655965cf94a7
Per Göttlicher <atlasfreak@gmx.de>
2024-12-10 14:00:53 +0100

    fix: calculate parallel cost for each player indiviually

--- src/main/java/hProjekt/model/Edge.java
+++ src/main/java/hProjekt/model/Edge.java
@@ -1,10 +1,13 @@
 package hProjekt.model;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import hProjekt.Config;
 import javafx.beans.property.Property;
@@ -154,25 +157,31 @@ public interface Edge {
      * @return the cost that needs to be paid to each player that has already built
      *         on this edge
      */
-    default int getParallelCost(Player player) {
+    default Map<Player, Integer> getParallelCost(Player player) {
+        final Map<Player, Integer> result = new HashMap<>();
         if (!getRailOwners().isEmpty() && !(getRailOwners().size() == 1 && getRailOwners().contains(player))) {
             if (Collections.disjoint(getHexGrid().getCities().keySet(), getAdjacentTilePositions())) {
-                return 5;
+                getRailOwners().stream().forEach(p -> result.put(p, 5));
+            } else {
+                getRailOwners().stream().forEach(p -> result.put(p, 3));
             }
-            return 3;
         }
 
-        if (getAdjacentTilePositions().stream()
-                .anyMatch(position -> {
+        getAdjacentTilePositions().stream()
+                .flatMap(position -> {
+                    if (getHexGrid().getCityAt(position) != null) {
+                        return Stream.empty();
+                    }
                     Set<Player> owners = getHexGrid().getTileAt(position).getEdges().stream()
                             .filter(Predicate.not(this::equals)).flatMap(edge -> edge.getRailOwners().stream())
-                            .collect(Collectors.toSet());
-                    return !owners.isEmpty() && !owners.contains(player);
-                })) {
-            return 1;
-        }
+                            .collect(Collectors.toUnmodifiableSet());
+                    if (owners.contains(player)) {
+                        return Stream.empty();
+                    }
+                    return owners.stream();
+                }).forEach(p -> result.put(p, Math.max(result.getOrDefault(p, 0), 1)));
 
-        return 0;
+        return result;
     }
 
     /**
@@ -183,7 +192,7 @@ public interface Edge {
      * @return the total cost that needs to be paid by the player to build a rail
      */
     default int getTotalParallelCost(Player player) {
-        return getParallelCost(player) * getRailOwners().size();
+        return getParallelCost(player).values().stream().reduce(0, Integer::sum);
     }
 
     /**
