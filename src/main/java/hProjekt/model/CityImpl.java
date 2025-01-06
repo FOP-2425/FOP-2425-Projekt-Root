fe6ca82f2018a0397119d7e2a7a850441861682e
Per Göttlicher <atlasfreak@gmx.de>
2025-01-06 20:21:28 +0100

    remove no longer existant attribute from javadoc

--- src/main/java/hProjekt/model/CityImpl.java
+++ src/main/java/hProjekt/model/CityImpl.java
@@ -6,7 +6,6 @@ package hProjekt.model;
  * @param position       the position of the city
  * @param name           the name of the city
  * @param isStartingCity whether the city is a starting city
- * @param rollNumbers    the roll numbers that belong to the city
  * @param hexGrid        the HexGrid instance this city is placed in
  */
 public record CityImpl(
